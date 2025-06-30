ddf5b872fac6815e8f380cb1921b344c8ba46fe8
NguyenDinhAn-2002 <an3g2002@gmail.com>
2025-06-30 22:54:41 +0700

    fix create quiz

--- src/services/quizManagement.ts
+++ src/services/quizManagement.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { QuizDetail } from '../types/quiz';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   withCredentials: true,
 });
 
